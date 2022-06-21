939ecd5b1310e11b9cf0eb9b19807679e85c3e74
Daniel Toll <ttoda@lnu.se>
2022-06-21 11:43:23 +0200

    till hobbe

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # The MIT License (MIT)
 # Copyright (c) 2018 Stephen Suffian
 
-
+"""
 import utime
 import run_sds011
 
@@ -14,3 +14,24 @@ while True:
     print('Air Quality PM2.5: {}, PM10: {}'.format(pm25, pm10))
     # make the socket blocking
     # (waits for the data to be sent and for the 2 receive windows to expire)
+"""
+import time
+from lib.bme import BME
+
+sda = 'P7'
+scl = 'P8'
+
+bme = BME((sda, scl), calibration_time=5)
+
+while True:
+    # Get air condition values
+    temp, humidity, pressure, air_quality = bme.get_values()
+
+    # Print all
+    print('Temperature:', temp, '°C')
+    print('Humidity:', humidity, '%')
+    print('pressure:', pressure, 'hPa')
+    print('air_quality:', air_quality, 'IAQ')
+    print()
+
+    time.sleep(1)
