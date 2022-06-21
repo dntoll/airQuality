cbbc9ae5aa31da329df9a2a588ae0841f48fe48f
Daniel Toll <ttoda@lnu.se>
2022-06-21 11:08:42 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# main.py -- put your code here!
+# The MIT License (MIT)
+# Copyright (c) 2018 Stephen Suffian
+
+
+import utime
+import run_sds011
+
+
+while True:
+    utime.sleep(1)
+    print('Attempt to Send')
+    pm25, pm10 = run_sds011.run_sensor()
+    print('Air Quality PM2.5: {}, PM10: {}'.format(pm25, pm10))
+    # make the socket blocking
+    # (waits for the data to be sent and for the 2 receive windows to expire)
